cbfebebad085fca0902285513e3d9c6e1ec0b3d1
AQ <50520937+Twsaiae@users.noreply.github.com>
2025-01-15 15:14:30 +0800

    东声最后一次更新

---
+++ python.py
@@ -0,0 +1,10 @@
+import torch
+# import mmcv
+
+# print(mmcv.__version__)
+print(torch.__version__)
+
+
+# python tools/test.py local_configs/segformer/B1/segformer.b1.512x512.ade.160k.py /models/segformer.b1.512x512.ade.160k.pth
+
+# pip install mmcv-full==1.5.1 -f https://download.openmmlab.com/mmcv/dist/cu101/torch1.7.1/index.html
